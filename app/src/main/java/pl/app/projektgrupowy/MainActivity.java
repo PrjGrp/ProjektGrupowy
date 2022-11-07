66c9fa364751e0b4506342ce0d729af7da8d1a59
patruch <80421521+mrptk@users.noreply.github.com>
2022-11-07 22:31:41 +0100

    Pierwsze podejscie do clienta REST

--- app/src/main/java/pl/app/projektgrupowy/MainActivity.java
+++ app/src/main/java/pl/app/projektgrupowy/MainActivity.java
@@ -2,13 +2,26 @@ package pl.app.projektgrupowy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import restcli.MockClient;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultsTextView;
+    private Button myButton;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultsTextView = (TextView) findViewById(R.id.results);
+        myButton = (Button) findViewById(R.id.button);
     }
-} // co to i to, jakos to zrobimy
+}
